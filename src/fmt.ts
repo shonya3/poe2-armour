85d05335d78c5d166fbf0e2ee7f500789f296e31
shonya3 <poeshonya3@gmail.com>
2024-12-30 16:01:58 +0300

    set 0 formatter as defaut

--- src/fmt.ts
+++ src/fmt.ts
@@ -1,11 +1,11 @@
-const formatter_0 = Intl.NumberFormat('ru', { maximumFractionDigits: 0 });
 const formatter_1 = Intl.NumberFormat('ru', { maximumFractionDigits: 1 });
+const formatter_0 = Intl.NumberFormat('ru', { maximumFractionDigits: 0 });
 export const fmt = (n: number, digits = 0) => {
 	switch (digits) {
-		case 0:
-			return formatter_0.format(n);
 		case 1:
-		default:
 			return formatter_1.format(n);
+		case 0:
+		default:
+			return formatter_0.format(n);
 	}
 };
